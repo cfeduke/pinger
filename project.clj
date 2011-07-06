c9de250fc62c11e6078c51a6b2c3fdb1556243d8
Charles Feduke <charles.feduke@gmail.com>
2011-07-06 14:20:19 -0400

    initial nonworking version

--- project.clj
+++ project.clj
@@ -1,3 +1,28 @@
-(defproject pinger "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject pinger "0.0.1-SNAPSHOT"
+  :description "A website availability tester"
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
+	:main pinger.core)
+
+(ns pinger.core
+	(:import (java.net URL))
+	(:gen-class))
+	
+(defn response-code [address]
+	(let [connection (.openConnection (URL. address))]
+	(doto connection
+		(.setRequestMethod "GET")
+		(.connect))
+	(.getResponseCode connection)))
+
+(defn available? [address]
+	(= 200 (response-code address)))
+	
+(defn -main []
+	(let [addresses '("http://www.somethingofthatilk.com"
+									  "http://amazon.com"
+										"http://google.com/badurl")]
+
+	(while true
+		(doseq [address addresses]
+			(println (available? address)))
+			(Thread/sleep (* 1000 60)))))
