1974adab938e74ceec45b9d51a31fb3e17d3553c
Charles Feduke <charles.feduke@gmail.com>
2011-07-07 12:44:06 -0400

    working version from article

--- project.clj
+++ project.clj
@@ -3,26 +3,3 @@
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
 	:main pinger.core)
 
-(ns pinger.core
-	(:import (java.net URL))
-	(:gen-class))
-	
-(defn response-code [address]
-	(let [connection (.openConnection (URL. address))]
-	(doto connection
-		(.setRequestMethod "GET")
-		(.connect))
-	(.getResponseCode connection)))
-
-(defn available? [address]
-	(= 200 (response-code address)))
-	
-(defn -main []
-	(let [addresses '("http://www.somethingofthatilk.com"
-									  "http://amazon.com"
-										"http://google.com/badurl")]
-
-	(while true
-		(doseq [address addresses]
-			(println (available? address)))
-			(Thread/sleep (* 1000 60)))))
--- src/pinger/core.clj
+++ src/pinger/core.clj
@@ -1,1 +1,23 @@
-(ns pinger.core)
+(ns pinger.core
+	(:gen-class)
+	(:import (java.net URL)))
+	
+(defn response-code [address]
+	(let [connection (.openConnection (URL. address))]
+	(doto connection
+		(.setRequestMethod "GET")
+		(.connect))
+	(.getResponseCode connection)))
+
+(defn available? [address]
+	(= 200 (response-code address)))
+	
+(defn -main []
+	(let [addresses '("http://www.somethingofthatilk.com"
+									  "http://amazon.com"
+										"http://google.com/badurl")]
+
+	(while true
+		(doseq [address addresses]
+			(println (available? address)))
+			(Thread/sleep (* 1000 60)))))
