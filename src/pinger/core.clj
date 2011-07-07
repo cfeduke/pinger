226f4cb8fe38ec00dba5a93c7fd88355c9963632
Charles Feduke <charles.feduke@gmail.com>
2011-07-07 13:03:41 -0400

    part two of the article

--- src/pinger/core.clj
+++ src/pinger/core.clj
@@ -1,5 +1,6 @@
 (ns pinger.core
 	(:gen-class)
+	(:require [pinger.scheduler :as scheduler])
 	(:import (java.net URL)))
 	
 (defn response-code [address]
@@ -12,12 +13,22 @@
 (defn available? [address]
 	(= 200 (response-code address)))
 	
-(defn -main []
+(defn check []
 	(let [addresses '("http://www.somethingofthatilk.com"
 									  "http://amazon.com"
 										"http://google.com/badurl")]
 
-	(while true
 		(doseq [address addresses]
-			(println (available? address)))
-			(Thread/sleep (* 1000 60)))))
+			(println (available? address)))))
+
+(def immediately 0)
+(def every-minute (* 60 1000))
+
+(defn start []
+  (scheduler/periodically check immediately every-minute))
+  
+(defn stop []
+  (scheduler/shutdown))
+  
+(defn -main []
+  (start))
